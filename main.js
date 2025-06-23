e5e3ff1ee8d714ca66228cf6b5f4da01a8bb7946
Vikash-0604 <vikashx0604@gmail.com>
2025-06-23 12:17:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+var typed = new Typed(".text", {
+    strings: ["Java Full Stack Developer"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
+
+var typed = new Typed(".text1", {
+    strings: ["Me"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
+var typed = new Typed(".text2", {
+    strings: ["Services"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
+var typed = new Typed(".text3", {
+    strings: ["Skills"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
+var typed = new Typed(".text4", {
+    strings: ["Projects"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
+
+var typed = new Typed(".text5", {
+    strings: ["Me"],
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
